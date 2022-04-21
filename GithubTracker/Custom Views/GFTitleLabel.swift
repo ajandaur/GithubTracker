5e0d65c7a1ab2135ce8b8b479f53ab8adf8f701a
ajandaur <anmol.jandaur@gmail.com>
2022-04-20 18:49:22 -0700

    Add UICollectionView

--- GithubTracker/Custom Views/GFTitleLabel.swift
+++ GithubTracker/Custom Views/GFTitleLabel.swift
@@ -40,4 +40,5 @@ class GFTitleLabel: UILabel {
         translatesAutoresizingMaskIntoConstraints = false
     }
     
+
 }
--- GithubTracker/Screens/FollowerListViewController.swift
+++ GithubTracker/Screens/FollowerListViewController.swift
@@ -10,13 +10,37 @@ import UIKit
 class FollowerListViewController: UIViewController {
     // Want to pass in a username to get the list of followers via network call
     var username: String!
+    var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureViewController()
+        configureCollectionView()
+        getFollowers()
+    
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    func configureViewController() {
         view.backgroundColor = .systemBackground
-        navigationController?.isNavigationBarHidden = false
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    func configureCollectionView() {
+        // initialize collectionView
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
         
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemRed
+        // register the cell
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
+    
+    func getFollowers() {
         // introduce completion handler via singleton
         NetworkManager.shared.getFollower(for: username, page: 1) { result in
             
@@ -31,11 +55,6 @@ class FollowerListViewController: UIViewController {
                 self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "Ok")
             }
         }
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.setNavigationBarHidden(false, animated: true)
     }
     
 }
